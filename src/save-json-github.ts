a6678e3ca80dbbec6559404c9408dee94983f5ca
cjmario89 <mario89cheng@gmail.com>
2025-02-26 18:12:37 +0800

    Updated price data

--- src/save-json-github.ts
+++ src/save-json-github.ts
@@ -85,8 +85,8 @@ async function main() {
   while (startDate < endDate) {
     console.log(`inserted ${startDate.toISOString()}`);
     await insertCoinPrice(startDate.getTime());
-    saveTimestampToLocal(startDate.getDate() + 1);
     startDate.setDate(startDate.getDate() + 1);
+    saveTimestampToLocal(startDate.getTime());
     break;
   }
   try {
